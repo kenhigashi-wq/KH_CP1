439f063305f2fa22558876d39f7d37245113f63e
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-08-21 10:51:12 -0600

    notes for 8/21/25

---
+++ notes_2nd/variables.py
@@ -0,0 +1,12 @@
+# KH 2nd variables notes
+# What is a variable? holds information for the programmer
+# We have to name our variable first. Then we use our assignment operator to tell its value. then we will set its content in the variable.
+# There are rules to names, they must not have spaces, use underscore instead. variables must begin with letters and cannot cotain special letters. The assignment operater is an equal sign by the way.
+student_count = 29
+amout_water = "ounce"
+name = "jake"
+grade = 98
+# Declaration is declaring the variable and saying it exists. Initialization comes after declaration, where you put the initial value in the variable.
+# to change the variable just type the variable and add the new value
+grade = 88
+print(grade)
