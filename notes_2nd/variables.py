7fc14f8eb2734cdf2e4fce607e29501ac0cdbab7
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-08-22 10:47:38 -0600

    who_are_you.py project and part of variables notes

--- notes_2nd/variables.py
+++ notes_2nd/variables.py
@@ -3,10 +3,12 @@
 # We have to name our variable first. Then we use our assignment operator to tell its value. then we will set its content in the variable.
 # There are rules to names, they must not have spaces, use underscore instead. variables must begin with letters and cannot cotain special letters. The assignment operater is an equal sign by the way.
 student_count = 29
-amout_water = "ounce"
+amount_water = "ounce"
 name = "jake"
 grade = 98
 # Declaration is declaring the variable and saying it exists. Initialization comes after declaration, where you put the initial value in the variable.
 # to change the variable just type the variable and add the new value
 grade = 88
 print(grade)
+#Q. What are the variables in this code: num1, num2, num4. Result, result2
+#Q. What's wrong with it? It's too boring and not unique. And also they are harder to remember. They are not specific.
---
+++ projects/who_are_you.py
@@ -0,0 +1,5 @@
+# KH who_are_you,py
+name = input("Whats your name?: ")
+age = input("Whats your age?: ")
+color = input("Whats your favorite color?: ")
+print("Hello", name, "I am", age, "too, and I also like", color, "too")
