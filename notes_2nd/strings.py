407bcbc3394cc341ae1646194b64b8eb0581e7c5
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-09-02 10:51:10 -0600

    strings

--- notes_2nd/strings.py
+++ notes_2nd/strings.py
@@ -1,2 +1,40 @@
 # KH 2nd Strings Notes
-# what is strings? Strings are a series of letter insede the quotation marks
+
+
+# what are strings? Strings are a collections of letters inside the quotation marks.
+# This "" and this '' are interchangeable.
+
+#Examples
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = "Xavier"
+month = 'september'
+food = "chocolate"
+last_name = "LaRusso"
+
+# If I need to use ' or "
+name = "la'Rose"
+best = '"The return of the king" is the best book ever!'
+
+funny = "the dog jumped in the lake"
+
+#why do we use strings?
+# Length of strings? Do 
+
+
+length = len(month)
+print("september has", length, "letter")
+
+# Escape Character
+print("Will said: \n'don\'t die'")
+
+#Concatenate
+full_name = first_name + " " + last_name
+print(full_name)
+
+# Index
+# Index is the location of a specific name
+
+# Slice
+print(last_name[2:6])
+
+string[start index: end index]
