9e692c53ffab6a6dc9099e9fb020e556c252d00d
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-09-05 10:49:24 -0600

    Idiot proof

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,1 +1,7 @@
 # KH 2nd idiot proof
+name = input("What is your name? ").strip().title()
+phone = input("What is your phone number? ").strip()
+phone = " ".join([phone[:-4][i:i+3] for i in range(0, len(phone) - 4, 3)]) + " " + phone[-4:]
+gpa = float(input("What is your GPA?: "))
+
+print(name, phone, gpa)
---
+++ starters/email.py
@@ -0,0 +1,2 @@
+# KH 2nd email starter
+# this code asks for email things and verifys if its valid or not
