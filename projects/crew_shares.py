695f70882572ba97635489661afc6eb04f28b086
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-09-15 10:15:56 -0600

    Crew share

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,4 +1,11 @@
 # KH 2nd crew share
-earned = input("How much was earned:")
-crew_mem = input("How many crew members are there (not including the captain and first mate):")
-Captain = earned/7
+earned = float(input("How much was earned:"))
+crew_mem = int(input("How many crew members are there (not including the captain and first mate):"))
+total_share = 7 + 3 + crew_mem
+one_share = earned / total_share
+captain = one_share * 7
+first = one_share * 3
+crew_guy = one_share - 500
+print("The captain gets: $" + str(round(captain, 2)))
+print("The first mate gets: $" + str(round(first, 2)))
+print("The crew still needs(individually): $" + str(round(crew_guy, 2)))
---
+++ starters/favnum.py
@@ -0,0 +1,4 @@
+#KH 2nd favorite number starter
+# No comma on the random_number1
+# No underscore for the very last variable
+# 
