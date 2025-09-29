869626a8b012d7b8397891cf9e1f872f84db6f46
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-09-29 10:51:08 -0600

    Notes for lists

---
+++ notes_2nd/lists.py
@@ -0,0 +1,21 @@
+# KH 2nd lists notes
+
+#import random
+
+#names = [ "alex", "ken","hi", "test"]
+
+#print(names)
+#print(names[3])
+
+#names[-1] = "Xavier"
+#names.extend(["wus53w","4q3waytf"])
+#print(names)
+
+#x = names.index("alex")
+#print(x)
+
+board = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+board[1][1] = "X"
+print(board)
