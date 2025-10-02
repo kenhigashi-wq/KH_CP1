cf4d36f7918add6a4cb118331310bb1e4ceb12e0
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-10-02 10:42:51 -0600

    mult table

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -1,8 +1,8 @@
 #KH 2nd Multiplication table
 
-num1 = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-for num in num1:
-        print("Table")
-    for i in range(1, num1)
-print(mult)
+
+
+for x in range(1, 13, 1):
+    print(f" {x * 1} \t {x*2} {x * 3} \t {x * 4} \t {x * 5} \t {x * 6} \t {x * 7} \t {x * 8} \t {x * 9} \t {x * 10} \t {x * 11} \t {x * 12} \t")
+   
