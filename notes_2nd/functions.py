3992670e2e314a6b7cb69106fbacbc95b7f649f3
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-10-21 10:11:20 -0600

    Functions

---
+++ notes_2nd/functions.py
@@ -0,0 +1,18 @@
+# KH 2nd period Functions Notes
+num = 0
+#parameters
+def add(x,y):
+    return x+y
+
+while num < add(4,4):
+    print("Duck")
+    num += 1
+print(f"result is {add(1,1)}")
+total = add(1,1)
+print(add(add(12,23)))
+
+
+def initials(name):
+    names  name.split(" ")
+    initial = ""            
+    for name in names 
