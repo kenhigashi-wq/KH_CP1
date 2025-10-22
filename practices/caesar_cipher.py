9a0074a500539a81439915726b4ea224e4b6c9c7
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-10-22 10:50:54 -0600

    new

--- practices/caesar_cipher.py
+++ practices/caesar_cipher.py
@@ -4,23 +4,26 @@ print(f"a = {ord("a")}")
 print(f"100 = {chr(100)}")
 
 # make function for the cipher
-def caesar_cipher(text, shift, mode):
+def encode(text, shift):
     result = ""
     for char in text:
         if char.isalpha():
             start = ord('a') if char.islower() else ord('A')# checks if upper case
-            if mode == "encrypt":# checks if encrypt and then encyptes
-                shifted_code = (ord(char) - start + shift) % 26 + start # does the thing 
-            elif mode == "decrypt":# same thing
-                 shifted_code = (ord(char) - start - shift) % 26 + start # also smae thing
-            else:# else statment
-                return "invalid Try again"
-
+            shifted_code = (ord(char) - start + shift) % 26 + start # does the thing 
             result += chr(shifted_code)
         else:
             result += char
     return result
-
+def decode(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            start = ord('a') if char.islower() else ord('A')# checks if upper case
+            shifted_code = (ord(char) - start - shift) % 26 + start # also smae thing
+            result += chr(shifted_code)
+        else:
+            result += char
+    return result
 # set message var
 message = input("Enter your message: ")
 #set the key var
@@ -30,10 +33,10 @@ op = input("Choose operation (1 for encode, 2 for decode): ")
     
 # make the option thing
 if op == "1":
-    encrypted_message = caesar_cipher(message, key, 'encrypt')
+    encrypted_message = encode(message, key)
     print(f"Encrypted message: {encrypted_message}")
 elif op == "2":# do it for decode too
-    decrypted_message = caesar_cipher(message, key, 'decrypt')
+    decrypted_message = decode(message, key)
     print(f"decrypted message: {decrypted_message}")
 else:# make else statement
     print("Invalid: Try again")
