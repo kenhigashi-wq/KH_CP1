5aeda7e4e4350d84f45e071e66819fc550bbf938
kenny <ken.higashi@ucas-edu.net>
2025-11-20 00:32:50 +0000

    hiy

---
+++ practices/flexible_calculator.py
@@ -0,0 +1,34 @@
+# KH, period 2, Flexible Calculator Assignment
+
+import statistics
+
+# Function to perform calculation based on operation
+def calculator(operation = "sum", *args):
+    # Check if numbers are provided
+    if len(args) == 0:
+        print("Invalid, type in something")
+    
+    # Perform math operation based on user choice
+if operation == "sum":#Make one for sum
+    total = 0#Start it at 0
+    for n in args
+    total += n
+    return total#return the total
+
+if operation == "average":
+    total = 0
+    for n in args
+    total += numbers
+    return total/len(args)
+
+elif operation == "sum":
+    total = 0
+    for n in args
+    total += numbers
+    return total
+
+elif operation == "max":
+    total = 0
+    for n in args
+    total += numbers
+    return total
