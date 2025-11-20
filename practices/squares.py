aea65fea65e6b014e04fcf2f86009c4cbada48ba
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-11-20 10:13:19 -0700

    hi

--- practices/squares.py
+++ practices/squares.py
@@ -1,1 +1,10 @@
 #Kh second period, squares practice
+#Make the variables for the numbers
+nums = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
+output = list(map(lambda n:(n**2), nums))#Make the exponential func
+#Print the output
+
+
+print(nums)
+print("all these numbers to teh power of 2 is")
+print(output)
