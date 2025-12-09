6ed1cd356257edac1fec0d632f558c265af4a9bc
kenhigashi-wq <ken.higashi@ucas-edu.net>
2025-12-09 10:46:34 -0700

    fin

---
+++ projects/final.py
@@ -0,0 +1,31 @@
+player_health = 1300
+player_strength = 200
+player_speed = 500
+player_charm = .2
+player_msp = 0
+msp_goal = 500
+#Inventory (Which is a dictionary)= {weapons: [], charm items: [], heal items: [], speed items: []}
+#Make a list for all items
+# rooms list = [kitchen, living room, bathroom, garage, Dining room, Hallway, Bedroom, library, Christmas tree room]
+#implement a list for visited that is empty(For now)
+def charm(enemy_name):
+    
+def using_item():
+
+def equip_item():
+
+def main_func():
+
+def start_game():
+    
+def change_room():
+
+def lose_game():
+    if player_health <= 0 and player_msp <= 500:
+        print("You got defeated! Your relative moves back in and destroy your peace.")
+def win_game():
+    if player_health >= 0 and player_msp >= 500:
+        print("You won the whole entire game! You home is peaceful, you now enjoy a cup of hot cocoa and relax")
+def room():
+
+def combat(enemy_health):
